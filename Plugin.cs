9d0d5ce0d3feb786a5a4fbe6d9c96de3cc662ac1
Ant2357 <ryu4979@gmail.com>
2025-05-10 03:40:05 +0900

    feat: init

---
+++ Plugin.cs
@@ -0,0 +1,52 @@
+using System;
+using BepInEx;
+using HarmonyLib;
+
+[BepInPlugin("ant2357.tithe_tax_mod", "Tithe Tax Mod", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        var harmony = new Harmony("ant2357.tithe_tax_mod");
+        harmony.PatchAll();
+    }
+}
+
+// 基礎税金を常に 0 にするパッチ
+[HarmonyPatch(typeof(Faction), "GetBaseTax")]
+public static class GetBaseTaxPatch
+{
+    private static void Postfix(ref int __result, bool evasion)
+    {
+        __result = 0;
+    }
+}
+
+// 有名税をプレイヤー所持金の10分の1に変更するパッチ
+[HarmonyPatch(typeof(Faction), "GetFameTax")]
+public static class GetFameTaxPatch
+{
+    private static void Postfix(ref int __result, bool evasion)
+    {
+        float playerMoney = EClass.pc.GetCurrency();
+        __result = (int)Math.Floor(playerMoney / 10f);
+    }
+}
+
+[HarmonyPatch(typeof(Scene))]
+[HarmonyPatch(nameof(Scene.Init))]
+class ScenePatch
+{
+    static void Postfix(Scene.Mode newMode)
+    {
+        if (newMode == Scene.Mode.StartGame)
+        {
+            Msg.NewLine();
+            Msg.SayRaw("=== Tithe Tax Mod ===");
+
+            Msg.NewLine();
+            int money = EClass.pc.GetCurrency();
+            Msg.SayRaw("現在の所持金は " + money + " オレンです。");
+        }
+    }
+}
